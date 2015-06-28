729ae513e680285a922d598b63c9cf5be98a2aaf
Alexandre Finger <alex.fingersb@gmail.com>
2015-06-28 18:01:25 -0300

    Configuraçāo spring

---
+++ src/main/java/br/com/it3/agent/Agent.java
@@ -0,0 +1,23 @@
+package br.com.it3.agent;
+
+import org.apache.camel.spring.Main;
+
+public class Agent {
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.setApplicationContextUri("file:conf/context.xml");
+		main.enableHangupSupport();
+		try {
+			System.out.println("Iniciando agent. Use CTRL + C para finalizar.\n");
+			main.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Finalizando agent");
+		
+	}
+	
+	
+}
